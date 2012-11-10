cf1e0b057f920057b83fb5ffc244b8e50a696856
Carl Jamilkowski <cdj255@nyu.edu>
2012-11-10 18:33:28 -0500

    form

--- application/controllers/text.php
+++ application/controllers/text.php
@@ -1,14 +1,32 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Text extends CI_Controller {
+class Home extends CI_Controller {
 
-	public function post_text()
+
+	public function index()
 	{
-		$this->load->model('sms_model');
-		$args = $this->input->post('args');
-		$phone = $this->input->post('phone_number_without_country_code');
-		$this->sms_model->process_text($args, $phone);
+		$this->load->helper(array('form', 'url'));
 
+		$this->load->library('form_validation');
+
+		$this->form_validation->set_rules('phone', 'add_number', 'required');			
+		$this->form_validation->set_rules('area', 'area', 'required');			
+		$this->form_validation->set_rules('age', 'age', '');			
+		$this->form_validation->set_rules('gender', 'gender', '');			
+		$this->form_validation->set_rules('question', 'question', '');
+		
+		$this->form_validation->set_error_delimiters('<br /><span class="error">', '</span>');
+
+		if ($this->form_validation->run() == FALSE)
+		{
+			$this->load->view('welcome_message');
+		}
+		else
+		{
+			$this->load->model('sms_model');
+			$this->sms_model->register_number(set_value('add_area'), set_value('add_number'), set_value('age'), set_value('gender'), set_value('myquestion'))
+			$this->load->view('welcome_message');
+		}
 	}
 }
 
